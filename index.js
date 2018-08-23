45ddec90335b1b0568124ff966985ef579055799
khuchuan <khuchuan@gmail.com>
2018-08-23 13:54:50 +0700

    update code register

--- index.js
+++ index.js
@@ -7,53 +7,53 @@ var user = require('./user');
 // Connect to db, save user
 mongoose.connect('mongodb://localhost/todoapps', function(err){
     if (err) throw err;
-    console.log('Connect db ok');
-
     // Connect ok, collect data and save db
 });
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.post('/register', function(req, res, next){
-    if ( req.body.username || req.body.email || req.body.password || req.body.passwordConf)    {
-        console.log('Vui long nhap du tham so');
-        next();
-    }
+app.use(bodyParser.json());
 
-    // Check pass and passConf
-    if (req.body.password != req.body.passwordConf)    {        
-        console.log('Password khong khop');
-        res.send('Password confirm khong khop');
-        return;
-    }
+app.post('/register', function(req, res, next){        
+    var obj = req.body;
+    console.log(obj);
     
-    const bcrypt = require('bcrypt');
-    var hask = bcrypt.hash(req.body.password, 10)    
+    const bcrypt = require('bcrypt');  
+
+    var hask = bcrypt.hash(obj.password, 10)    
     .then(function(hashedPassword) {
         var userInfo = new user({
-            email: req.body.email,
-            username: req.body.username,
+            email: obj.email,
+            username: obj.username,
             password: hashedPassword
         });    
+
         userInfo.save(function(err){
             if (err) throw err;
-            console.log('Register user successfulee');
+            
+            console.log('Register user successfull');
+
+            // res.setHeader("Content-Type", "text/html");
+            // res.write("<p>Hello World</p>");
+            // res.end();  
+
+            res.send("<p>Register success</p>");            
         })        
     })
     .then(function() {
-        res.send('OK');
+        console.log('da vao then');               
     })
     .catch(function(error){
-        console.log("Error saving user: ");
-        console.log(error);
-        next();
-    });
-   
+        console.log("Error saving user: " + JSON.stringify(error));                        
+    });   
 });
 
 app.use(express.static(__dirname + '/templateLogReg'));
 app.get('/', function(req, res){
     res.sendFile (path.join(__dirname + '/templateLogReg/index.html'));
+})
+
+app.get('/alert', function(req, res){
+    res.end("Register ok");
 })
 
 app.listen(3000, function(){
